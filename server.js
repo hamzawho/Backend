c8de271d26c020f234e9779fd253e483c333153a
hamzawho <hmasood369@gmail.com>
2024-08-20 18:08:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
   host: 'rockhair-saloon.cx2goocy64gl.eu-north-1.rds.amazonaws.com',
   user: 'admin', 
   password: 'U4m2nj10', 
-  database: 'rockhairsaloon ',
+  database: 'rockhairsaloon',
   port: '3306',
 });
 
