56f1e504ed6c5d60ba52621d4b7d44685728c2d9
hamzawho <hmasood369@gmail.com>
2024-08-30 16:23:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 // app.use(cors());
 app.use(cors({
-  origin: 'http://thedemoapp.online', // Replace with your actual frontend URL
+  origin: 'http://thedemoapp.online/', // Replace with your actual frontend URL
 }));
 
 app.use(express.json());
